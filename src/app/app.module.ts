170b784208be855522f399e46a357e32cf11d9e7
RomanivRoma <romaniv.roma2013@gmail.com>
2022-06-01 18:13:01 +0300

    feat: create auth module, new pages with email vaidation and code verification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './features/auth/auth.module';
+import { LoginComponent } from './features/auth/pages/login/login.component';
 import { ButtonsModule } from './shared/components/buttons/buttons.module';
 import { ChipsModule } from './shared/components/chips/chips.module';
 import { FabsModule } from './shared/components/fabs/fabs.module';
@@ -11,10 +14,11 @@ import { LogoModule } from './shared/components/logo/logo.module';
 import { ToastModule } from './shared/components/toast/toast.module';
 import { TooltipModule } from './shared/components/tooltip/tooltip.module';
 import { SharedModule } from './shared/shared.module';
+import { SpinnerComponent } from './core/components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,11 +29,13 @@ import { SharedModule } from './shared/shared.module';
     FabsModule,
     ButtonsModule,
     ChipsModule,
-    InputsModule,
-    LogoModule,
-    ToastModule
+
+    ToastModule,
+    AuthModule,
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: []
 })
 export class AppModule { }
